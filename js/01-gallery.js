3a1688cc9acbb950249064d43a07d5acf60ad0d1
Grygor Sergii <103570371+Sergg707@users.noreply.github.com>
2023-08-05 19:06:04 +0300

    add simple-lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,53 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const imgMarkup = createGalleryItemMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', imgMarkup);
+
+function createGalleryItemMarkup(gallary){
+   return gallary.map(({preview, original, description}) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="Image ${description}"
+      />
+    </a>
+  </li>`
+   }).join("")
+};
+
+galleryRef.addEventListener('click', onClickImg)
+let modalWindow
+function onClickImg(evt) {
+  evt.preventDefault();
+  const galleryItem = evt.target.closest('.gallery__item')
+  if (!galleryItem) {
+    return
+  };
+
+  openModalWindow(evt)
+};
+function openModalWindow(evt) {
+  modalWindow = basicLightbox.create(`
+  <img src="${evt.target.dataset.source}">`)
+  modalWindow.show()
+}
+
+galleryRef.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    modalWindow.close()
+  }
+})
+
+
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery')
+const greateImgMarkup = createGalleryItemMarkup(galleryItems)
+
+
+galleryRef.insertAdjacentHTML('beforeend', greateImgMarkup)
+
+function createGalleryItemMarkup(gallery) {
+        return gallery.map(({preview, original, description}) => {
+           return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+               <img class="gallery__image" src="${preview}" alt="Image ${description}" />
+            </a>
+         </li>`
+        }).join('')
+
+}
+var lightbox = new SimpleLightbox('.gallery a', {  captionsData: 'alt', captionDelay: 250, });
